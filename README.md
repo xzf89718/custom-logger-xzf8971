9d507ac731fce3b5af1f8a274cae99328d80baa9
ZifengXu <zifeng.xu@cern.ch>
2022-11-07 17:10:51 +0800

    Init commit v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # custom-logger-xzf8971
 Just a very simple wrapper to configure a thread safe logger.
+
+# Usage
+<font color='red'>The logger_name must be exclusive!</font>
+```python
+from custom_logger import CustomLoggerWrapper
+
+loggerWrapper = CustomLoggerWrapper(logger_name="example_logger" logger_level=logging.INFO, logger_propagate=False, logger_fmt="%(asctime)s - %(levelname)s - %(message)s", log_filename="mylog.log")
+loggerWrapper.InitLogger()
+logger = loggerWrapper.GetInitedLogger()
+logger.info("example")
+```
---
+++ custom_logger/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+
+class CustomLoggerWrapper():
+
+    def __init__(self, logger_name, logger_level=logging.INFO, logger_propagate=False, logger_fmt="%(asctime)s - %(levelname)s - %(message)s", log_filename="mylog.log"):
+        self.custom_logger = None
+        self.logger_name = logger_name
+        self.logger_propagate = logger_propagate
+        self.logger_level = logger_level
+        self.logger_fmt = logger_fmt
+        self.log_filename = log_filename
+
+    def InitLogger(self):
+        self.custom_logger = logging.getLogger(self.logger_name)
+        self.custom_logger.propagate = False
+        self.custom_logger.setLevel(level=logging.INFO)
+        logger_fmt = logging.Formatter(self.logger_fmt)
+        # Add file_handler
+        file_handler = logging.FileHandler(self.log_filename)
+        file_handler.setFormatter(logger_fmt)
+        self.custom_logger.addHandler(file_handler)
+
+    def GetInitedLogger(self):
+        return logging.getLogger(self.logger_name)
